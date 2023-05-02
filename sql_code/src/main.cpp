08728966f559bac8a95b22001dc76b22245d525e
alex_mac <lll@>
2023-05-02 21:50:11 +0300

    Merge main

--- sql_code/src/main.cpp
+++ sql_code/src/main.cpp
@@ -5,25 +5,25 @@
 
 int main() 
 {
-    // main_test();
+    main_test();
 
-    // DataBase& main_base = DataBase::get_instance();
-    // Person per("Name 1", "");
-    // Person per1("Name 1", "Psw 1");
-    // Person per2("Name 12", "Psw 1");
-    // Event ev1("First event");
-    // main_base.add_event(ev1);
+    DataBase& main_base = DataBase::get_instance();
+    Person per("Name 1", "");
+    Person per1("Name 1", "Psw 1");
+    Person per2("Name 12", "Psw 1");
+    Event ev1("First event");
+    main_base.add_event(ev1);
 
-    // std::vector<Event>all_ev = main_base.get_all_events();
+    std::vector<Event>all_ev = main_base.get_all_events();
     
-    // std::cout << "All events:\n";
-    // for (const auto& item : all_ev)
-    // {
-    //     std::cout << item << std::endl;
-    // }
-    // Event found = main_base.get_event(ev1);
-    // std::cout << "Found event:\n";
-    // std::cout << found;
+    std::cout << "All events:\n";
+    for (const auto& item : all_ev)
+    {
+        std::cout << item << std::endl;
+    }
+    Event found = main_base.get_event(ev1);
+    std::cout << "Found event:\n";
+    std::cout << found;
 
     // main_base.add_person(per);
     // main_base.add_person(per);
