33f762ceac3846c26f755b6416ea95615ded59f7
nik0zer <s21b1.nikozer@gmail.com>
2023-05-04 01:15:03 +0300

    #

--- server_main.cpp
+++ server_main.cpp
@@ -4,12 +4,28 @@
 
 void handler(ReadData read_data)
 {
-    std::cout<<"\nname:\n"<<read_data.data_name()<<"\nbuffer:\n"<<read_data.data_str()<<std::endl;
+    if(DataBase::get_instance().parse_cmd(read_data.data_name(), read_data.data_str()) == 0)
+    {
+        return;
+    }
+
+    if(read_data.data_name() == "send_me_actual_persons")
+    {
+        //return std::vector<Event>
+    }
+
+    if(read_data.data_name() == "send_me_actual_events")
+    {
+        //return std::vector<Event>
+    }
+
+    
+
 }
 
 void client_session(ClientConnection client_connection)
 {
-    client_connection.read_data(handler);
+    client_connection.thread_cycle_read(handler);
 }
 
 
